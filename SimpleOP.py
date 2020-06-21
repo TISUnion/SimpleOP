a91b962b6afba4af2846f10281ff9929c5629148
Fallen_Breath <yqx791125833@gmail.com>
2020-06-21 13:00:46 +0800

    Update SimpleOP.py

--- SimpleOP.py
+++ SimpleOP.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
+def on_user_info(server, info):
+	if server.get_permission_level(info) > 0:
+		if info.content == '!!op' and info.is_player:
+			server.execute('op ' + info.player)
+		if info.content == '!!restart':
+			server.restart()
 
-def on_info(server, info):
-	if info.is_player and info.content == '!!op':
-		server.execute('op ' + info.player)
-	if info.content == '!!restart':
-		server.restart()
+def on_load(server, old):
+	server.add_help_message('!!op', '获取op')
+	server.add_help_message('!!restart', '重启服务器')
