cfdb5d90692a38b8ea2c312c130364fe8483e8a0
Fallen_Breath <yqx791125833@gmail.com>
2021-01-15 23:37:53 +0800

    stop spam

--- SimpleOP.py
+++ SimpleOP.py
@@ -1,4 +1,5 @@
 import time
+from threading import Lock
 
 from mcdreforged.api.all import *
 
@@ -11,13 +12,22 @@ PLUGIN_METADATA = {
 	'link': 'https://github.com/MCDReforged/SimpleOP'
 }
 
+restart_lock = Lock()
+
 
 @new_thread(PLUGIN_METADATA['name'] + ' - restart')
 def restart(source: CommandSource):
-	for i in range(5):
-		source.get_server().say(RText('{} 秒后重启服务器!'.format(5 - i), color=RColor.red))
-		time.sleep(1)
-	source.get_server().restart()
+	acq = restart_lock.acquire(blocking=False)
+	if not acq:
+		source.reply('已经在重启了')
+		return
+	try:
+		for i in range(5):
+			source.get_server().say(RText('{} 秒后重启服务器!'.format(5 - i), color=RColor.red))
+			time.sleep(1)
+		source.get_server().restart()
+	finally:
+		restart_lock.release()
 
 
 def give_op(source: CommandSource):
@@ -26,6 +36,12 @@ def give_op(source: CommandSource):
 
 
 def on_load(server: ServerInterface, prev):
+	try:
+		global restart_lock
+		restart_lock = prev.restart_lock
+	except AttributeError:
+		pass
+
 	server.register_help_message('!!op', '给我op')
 	server.register_help_message('!!restart', '重启服务器，延迟5秒')
 	server.register_command(Literal('!!op').runs(give_op))
