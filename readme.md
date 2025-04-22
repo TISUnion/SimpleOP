5e32e7e9ca2a26d2e07297887f88d31c57d09457
Fallen_Breath <nellaforax@outlook.com>
2025-04-23 01:45:43 +0800

    A few more configs

--- readme.md
+++ readme.md
@@ -1,7 +1,20 @@
+# SimpleOP
+
 `!!op` give you op
 
 `!!restart` restart the server
 
 Mostly used in creative servers
 
-Config file: `config/simple_op/config.json`, for restart delay (default 5 seconds)
+Config file: `config/simple_op/config.json`
+
+```json5
+{
+    "enabled": true,     // The global switch of the plugin
+    "restart_delay": 5,  // `!!restart` restart countdown duration in second
+    "permissions": {     // command minimum required permissions
+        "op": 1,
+        "restart": 1
+    }
+}
+```
--- simple_op/__init__.py
+++ simple_op/__init__.py
@@ -4,13 +4,21 @@ from threading import Lock
 from mcdreforged.api.all import *
 
 
+class PermissionConfig(Serializable):
+	op: int = 1
+	restart: int = 1
+
+
 class Config(Serializable):
+	enabled: bool = True
 	restart_delay: int = 5
+	permissions: PermissionConfig = PermissionConfig()
 
 
 PLUGIN_METADATA = ServerInterface.get_instance().as_plugin_server_interface().get_self_metadata()
 restart_lock = Lock()
 config = Config.get_default()
+config.enabled = False
 
 
 def tr(translation_key: str, *args, **kwargs) -> RTextMCDRTranslation:
@@ -19,6 +27,10 @@ def tr(translation_key: str, *args, **kwargs) -> RTextMCDRTranslation:
 
 @new_thread(PLUGIN_METADATA.name + ' - restart')
 def restart(source: CommandSource):
+	if source.get_permission_level() < config.permissions.restart:
+		source.reply(tr('permission_denied'))
+		return
+
 	acq = restart_lock.acquire(blocking=False)
 	if not acq:
 		source.reply(tr('restart.spam'))
@@ -33,6 +45,10 @@ def restart(source: CommandSource):
 
 
 def give_op(source: CommandSource):
+	if source.get_permission_level() < config.permissions.op:
+		source.reply(tr('permission_denied'))
+		return
+
 	if isinstance(source, PlayerCommandSource):
 		source.get_server().execute('op {}'.format(source.player))
 	else:
@@ -49,7 +65,10 @@ def on_load(server: PluginServerInterface, prev):
 
 	global config
 	config = server.load_config_simple(target_class=Config)
-	server.register_help_message('!!op', tr('help.op'))
-	server.register_help_message('!!restart', tr('help.restart', config.restart_delay))
-	server.register_command(Literal('!!op').runs(give_op))
-	server.register_command(Literal('!!restart').runs(restart))
+	if not config.enabled:
+		server.logger.info('{} is disabled by config'.format(PLUGIN_METADATA.name))
+
+	server.register_help_message('!!op', tr('help.op'), permission=config.permissions.op)
+	server.register_help_message('!!restart', tr('help.restart', config.restart_delay), permission=config.permissions.restart)
+	server.register_command(Literal('!!op').runs(give_op).precondition(Requirements.has_permission(config.permissions.op)))
+	server.register_command(Literal('!!restart').runs(restart).precondition(Requirements.has_permission(config.permissions.restart)))
